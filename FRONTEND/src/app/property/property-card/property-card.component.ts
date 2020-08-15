0a4b0f42ee833b39af1918556de72d581a0e80d9
Andy Anunciacion <andyanunciacion@yahoo.com>
2020-08-16 00:16:50 +0800

    commit

---
+++ FRONTEND/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Type } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent implements OnInit {
+
+  Person: any = {
+    "Name": "Andy Anunciacion",
+    "Age": 23,
+    "Gender": "Male",
+    "Address": "Angeles City"
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
